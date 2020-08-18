5309940bc571922194429c2e24b9ac4c3b1b2e81
ericrsmith35 <ericrsmith35@gmail.com>
2020-08-18 17:19:53 -0400

    Remove datatableV2 from this package

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/resourceUrl/BulkDeleteImage" {
+    var BulkDeleteImage: string;
+    export default BulkDeleteImage;
+}
+declare module "@salesforce/resourceUrl/DeleteTrash" {
+    var DeleteTrash: string;
+    export default DeleteTrash;
+}
+declare module "@salesforce/resourceUrl/OneViewBanner" {
+    var OneViewBanner: string;
+    export default OneViewBanner;
+}
